1c815464ea5bb5a13408c4fe1b5098150ce808dc
samironbhowmik <bhowmiksamiron@gmail.com>
2023-04-25 20:53:45 +0530

    changes applied

--- src/compoents/bookmark.jsx
+++ src/compoents/bookmark.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-
+import React, { useState, useEffect } from "react";
+import "../compoents/bookmark.css"
 function Bookmark() {
+  const [bookmarkList, setBookmarkList] = useState([]);
+
+  useEffect(() => {
+    const bookmarkData = JSON.parse(localStorage.getItem("bookmark")) || [];
+    setBookmarkList(bookmarkData);
+  }, []);
+
   return (
-    <div>Bookmark</div>
-  )
+    <div>
+      <div className="bookmark-container">
+        <h1 className="heading">Bookmarks</h1>
+        <div className="bookmark-results">
+          {bookmarkList?.map((item, id) => {
+            return (
+              <div className="images" key={id}>
+                <img src={item.urls.small} alt="" />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Bookmark
+export default Bookmark;
--- src/compoents/photoSearch.jsx
+++ src/compoents/photoSearch.jsx
@@ -1,57 +1,74 @@
-import React, { useState } from 'react'
-import "../compoents/photoSearch.css"
-import axios from 'axios'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import '../compoents/photoSearch.css';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 function PhotoSearch() {
+  const [search, setSearch] = useState('');
+  const [list, setList] = useState([]);
+  
+  const API_KEY = 'bI8soWbQXsVt3jlOVX6mAOi0q6Lc8bVAggHtuO3oPGc';
 
-    const [search, setSearch] = useState("")
-    const [list, setList] = useState([])
-    const [bookmark, setBookmark] = useState([])
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const result = await axios.get(
+      `https://api.unsplash.com/search/photos?page=1&query=${search}&client_id=${API_KEY}`
+    );
+    setList(result.data.results);
+  };
 
-    const API_KEY = "bI8soWbQXsVt3jlOVX6mAOi0q6Lc8bVAggHtuO3oPGc"
-
-    const handleSearch = async(e)=>{
-        e.preventDefault()
-        // console.log("working");
-        const result = await axios.get(`https://api.unsplash.com/search/photos?page=1&query=${search}&client_id=${API_KEY}`)
-        // console.log(search);
-        console.log(result.data.results);
-        setList(result.data.results)
-    }
+  const handleBookmark = (item) => {
+    const bookmarkData = JSON.parse(localStorage.getItem('bookmark')) || [];
+    bookmarkData.push(item);
+    localStorage.setItem('bookmark', JSON.stringify(bookmarkData));
+  };
 
   return (
     <div>
-        <div className="container">
-            <div className="heading">
-                <div className="left">
-                    <h1>React Photo Search</h1>
-                </div>
-                <div className="right">
-                    <Link to="/bookmark ">
-                    <button className='bookmark-btn'>Bookmarks</button>
-                    </Link>
-                </div>
-            </div>
-            <form action="" className="input-form">
-                <input className='search' type="text" placeholder='Search free high resolution images' onChange={(e)=>{setSearch(e.target.value)}} value={search}/>
-                <button className='search-btn' onClick={handleSearch}>Search</button>
-            </form>
-
-            <div className="results">
-                {list?.map((item,id)=>{
-                    return(
-                        <div className='images' key={id}>
-                            <img src={item.urls.small} alt="" />
-                            {/* <button>Bookmark</button> */}
-                        </div>
-                    )
-                })}
-            </div>
-
+      <div className="container">
+        <div className="heading">
+          <div className="left">
+            <h1>React Photo Search</h1>
+          </div>
+          <div className="right">
+            <Link to="/bookmark ">
+              <button className="bookmark-btn">Bookmarks</button>
+            </Link>
+          </div>
         </div>
+        <form action="" className="input-form">
+          <input
+            className="search"
+            type="text"
+            placeholder="Search free high resolution images"
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+            value={search}
+          />
+          <button className="search-btn" onClick={handleSearch}>
+            Search
+          </button>
+        </form>
+
+        <div className="results">
+          {list?.map((item, id) => {
+            return (
+              <div className="images" key={id}>
+                <img src={item.urls.small} alt="" />
+                <button
+                  className="img-button"
+                  onClick={() => handleBookmark(item)}
+                >
+                  Bookmark
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default PhotoSearch
+export default PhotoSearch;
